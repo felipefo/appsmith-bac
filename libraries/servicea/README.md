d2e4369ff829b3c693f4d605ecaa793cd64206e3
felipefo <felipefo@users.noreply.github.com>
2024-03-30 10:18:42 -0300

    .
    .

---
+++ libraries/servicea/README.md
@@ -0,0 +1,16 @@
+# servicea
+
+## Usage
+
+You can use the JSDelivr CDN to import this custom library into Appsmith.
+```sh
+https://cdn.jsdelivr.net/gh/appsmithorg/foundry@main/dist/servicea.umd.js
+```
+
+## Contributing
+
+Contributions are always welcome!
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ libraries/servicea/index.js
@@ -0,0 +1,16 @@
+
+export default {
+// index.js
+
+/**
+ * Converte uma string para maiúsculas
+ * @param {string} str A string a ser convertida
+ * @returns {string} A string convertida para maiúsculas
+ */
+ toUpperCase(str) {
+  return str.toUpperCase();
+}
+
+// Exporte a função toUpperCase para ser utilizada em outros arquivos
+
+};
