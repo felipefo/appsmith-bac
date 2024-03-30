d2407763139f5c8cd993628651ef1e77f39d8f2f
felipefo <felipefo@users.noreply.github.com>
2024-03-30 14:40:09 -0300

    .
    .

--- libraries/servicea/index.js
+++ libraries/servicea/index.js
@@ -2,14 +2,38 @@
 export default {
 // index.js
 
-/**
- * Converte uma string para maiúsculas
- * @param {string} str A string a ser convertida
- * @returns {string} A string convertida para maiúsculas
- */
- toUpperCase(str) {
-  return str.toUpperCase();
+teste(){
+	
+},
+ async getAnimais(){
+	
+// URL da API que retorna JSON
+  const apiUrl = 'http://localhost:2337/api/animals';
+ // Função para fazer a requisição usando Fetch API
+ // Faz a requisição para a API usando fetch()
+ 
+  var dados = await fetch(apiUrl)
+    .then(response => {
+      // Verifica se a resposta da requisição é bem-sucedida (status 200)
+      if (!response.ok) {
+        throw new Error('Erro ao carregar os dados');
+      }
+      // Se a resposta for bem-sucedida, retorna os dados no formato JSON
+      return response.json();
+    })
+    .then(data => {
+      // Manipula os dados recebidos
+      console.log('Dados recebidos:', data);
+      // Aqui você pode processar os dados como desejar
+	  return data;
+    })
+    .catch(error => {
+      // Captura qualquer erro ocorrido durante a requisição
+      console.error('Erro:', error.message);
+    });
+	return dados; 
 }
+
 
 // Exporte a função toUpperCase para ser utilizada em outros arquivos
 
